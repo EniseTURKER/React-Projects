75681dac1316dd6a942ba76ad3682805a59a6990
EniseTurker <eniseturker09@gmail.com>
2022-02-08 22:57:47 +0300

    react style projesi eklendi

---
+++ 02-Styling-Project/src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import MyPoems from "./MyPoems";
+import {styleSheet} from "../styles";
+
+const divStyle = {
+    background: styleSheet.background
+}
+
+export default function App() {
+  return (
+    <div style={divStyle}>
+      <h1 style={{color:"white",padding:"2em"}}>Welcome To My Page</h1>
+      <MyPoems />
+      <footer style={{color:"white",fontSize:"4em",padding:"1em"}}>Thank You Visiting My Page!</footer>
+    </div>
+
+  );
+}
