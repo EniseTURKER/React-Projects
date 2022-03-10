63d974ec210665c33ac774e3a013e57bb602fc12
EniseTurker <eniseturker09@gmail.com>
2022-03-10 10:00:46 +0300

    new.

--- react.counter.proje/src/components/Counter.js
+++ react.counter.proje/src/components/Counter.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 
 export default class Counter extends Component {
   state = { count: 0 }
+  
   increase = () => this.setState({ count: ++this.state.count })
   decrease=()=> this.setState({count:--this.state.count})
   
   render() {
     return <div className='card' >
-      <h1>{ this.state.count}</h1>
-      <button onClick={this.increase}>increase(+)</button>
-      <button onClick={this.decrease}>decrease(-)</button>
-
+      <h1 className='pop'>{ this.state.count}</h1>
+      <br></br>
+      <div >
+      <button className='corn' onClick={this.increase}>increase(+)</button>
+      <button className='corn' onClick={this.decrease}>decrease(-)</button>
+      </div>
     </div>;
   }
 }
